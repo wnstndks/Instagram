b3b8cbce3e616f4e9c3696dc4ee55ef995a0462a
wnstndks <anjs9912@gmail.com>
2024-09-22 21:33:21 +0900

    footer layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import './styles/globals.css'
-import type { Metadata } from 'next'
-import Header from './components/Header'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './styles/globals.css';
+import type { Metadata } from 'next';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
 export const metadata: Metadata = {
   title: 'InstaClone',
   description: 'Instagram clone created with Next.js',
@@ -17,6 +19,7 @@ export default function RootLayout({
       <body className="bg-gray-100">
         <Header />
         <main>{children}</main>
+        <Footer/>
       </body>
     </html>
   )
